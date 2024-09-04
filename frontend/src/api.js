fb6ca46bb9dda443eb5dd9daeb6051e1694bdcf4
ctrlKshav <keshavm9978@gmail.com>
2024-09-05 00:46:20 +0530

    added backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 ﻿import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl="https://todo-5zj3.onrender.com"
+
 const api = axios.create({
-    baseURL : import.meta.env.VITE_API_URL
+    baseURL : import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 })
 
 api.interceptors.request.use(
