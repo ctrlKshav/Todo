9285c0aa0488578b8823826b28d936b38c5a2565
ctrlKshav <keshavm9978@gmail.com>
2024-09-05 15:48:57 +0530

    Revert "protected route on logout"
    This reverts commit 69656a0adb2b02bdaff093f7bd553d6039cd3c24.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
           }
         />
         <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<ProtectedRoute><Logout /></ProtectedRoute>} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
