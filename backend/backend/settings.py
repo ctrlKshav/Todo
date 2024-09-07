3191840f1530a88ea60967b8d479fa97063dedc9
ctrlKshav <keshavm9978@gmail.com>
2024-09-07 15:49:14 +0530

    backend stuff

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,11 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG",'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
+# ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
